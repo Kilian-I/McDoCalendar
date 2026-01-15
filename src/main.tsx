8b5f845d620339da16ccafced7d8c54074eec660
Kilian-I <ide.kilianpro@gmail.com>
2026-01-15 21:12:00 +0100

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { BrowserRouter } from 'react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 minutes par défaut
+      refetchOnWindowFocus: false, 
+    },
+  },
+});
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+     <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>  
+      </QueryClientProvider>
+  </StrictMode>,
+)
