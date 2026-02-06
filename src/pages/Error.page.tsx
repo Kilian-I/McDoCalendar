9a28763d31d5ed6ded60d4d3160e1caa30a0d0c3
Kilian-I <ide.kilianpro@gmail.com>
2026-02-06 10:00:29 +0100

    desin error page

--- src/pages/Error.page.tsx
+++ src/pages/Error.page.tsx
@@ -1,12 +1,19 @@
 //src/pages/Error.page.tsx
 
-
-export default function ErrorPage() {
+const ErrorPage = () => {
   return (
-    <>
-        <div>
-            <h1>Error 404</h1>
-        </div>
-    </>
-  )
-}
+    <div className="flex flex-col items-center justify-center h-screen font-sans text-center">
+      <h1 className="text-6xl font-bold">404</h1>
+      <p className="text-xl text-gray-600 mt-4">Oups ! Page introuvable.</p>
+      
+      <a 
+        href="/" 
+        className="mt-6 px-4 py-2 bg-black text-white rounded hover:bg-gray-800 transition"
+      >
+        Retour à l'accueil
+      </a>
+    </div>
+  );
+};
+
+export default ErrorPage;
